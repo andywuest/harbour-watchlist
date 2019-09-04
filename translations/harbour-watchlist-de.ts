8830d2071fa9de8f0b0821b8cc70158567a95144
andywuest <andreas.wuest.freelancer@gmail.com>
2019-09-04 21:58:51 +0200

    added

---
+++ translations/harbour-watchlist-de.ts
@@ -0,0 +1,167 @@
+<?xml version="1.0" encoding="utf-8"?>
+<!DOCTYPE TS>
+<TS version="2.1">
+<context>
+    <name>AboutPage</name>
+    <message>
+        <location filename="../qml/pages/AboutPage.qml" line="36"/>
+        <source>Reset Database</source>
+        <translation>Datenbank zurücksetzen</translation>
+    </message>
+    <message>
+        <location filename="../qml/pages/AboutPage.qml" line="49"/>
+        <location filename="../qml/pages/AboutPage.qml" line="62"/>
+        <source>About Watchlist</source>
+        <extracomment>AboutPage title - header
+----------
+AboutPage title - about text title</extracomment>
+        <translation>Über Watchlist</translation>
+    </message>
+    <message>
+        <location filename="../qml/pages/AboutPage.qml" line="64"/>
+        <source>This is app is a simple stock watchlist for Sailfish OS. Watchlist is open source and licensed under the GPL v3.</source>
+        <extracomment>AboutPage text - about text</extracomment>
+        <translation type="unfinished">Diese Anwendung ist eine kleine Aktien-Watchlist für Sailfish OS. Watchlist ist Open Source und unter der GPL v3 lizensiert.</translation>
+    </message>
+    <message>
+        <location filename="../qml/pages/AboutPage.qml" line="74"/>
+        <source>Version</source>
+        <extracomment>AboutPage version label</extracomment>
+        <translation>Version</translation>
+    </message>
+    <message>
+        <location filename="../qml/pages/AboutPage.qml" line="85"/>
+        <source>Author</source>
+        <extracomment>AboutPage author label</extracomment>
+        <translation>Autor</translation>
+    </message>
+    <message>
+        <location filename="../qml/pages/AboutPage.qml" line="106"/>
+        <source>Source code</source>
+        <extracomment>AboutPage about source label</extracomment>
+        <translation>Quellcode</translation>
+    </message>
+</context>
+<context>
+    <name>AddStockPage</name>
+    <message>
+        <location filename="../qml/pages/AddStockPage.qml" line="63"/>
+        <source>Network error</source>
+        <extracomment>AddStockPage network error</extracomment>
+        <translation>Netzwerkfehler</translation>
+    </message>
+    <message>
+        <location filename="../qml/pages/AddStockPage.qml" line="106"/>
+        <source>Search Results</source>
+        <extracomment>AddStockPage search result header</extracomment>
+        <translation>Suchergebnisse</translation>
+    </message>
+    <message>
+        <location filename="../qml/pages/AddStockPage.qml" line="113"/>
+        <source>Find your Stock...</source>
+        <extracomment>AddStockPage search result input field</extracomment>
+        <translation>Finde deine Aktie...</translation>
+    </message>
+    <message>
+        <location filename="../qml/pages/AddStockPage.qml" line="149"/>
+        <source>No results found</source>
+        <extracomment>AddStockPage no results label</extracomment>
+        <translation>Keine Ergebnisse gefunden</translation>
+    </message>
+    <message>
+        <location filename="../qml/pages/AddStockPage.qml" line="186"/>
+        <source>Add</source>
+        <extracomment>AddStockPage add menu item</extracomment>
+        <translation>Hinzufügen</translation>
+    </message>
+</context>
+<context>
+    <name>CoverPage</name>
+    <message>
+        <location filename="../qml/cover/CoverPage.qml" line="95"/>
+        <source>Top</source>
+        <translation>Gewinner</translation>
+    </message>
+    <message>
+        <location filename="../qml/cover/CoverPage.qml" line="95"/>
+        <source>Flop</source>
+        <translation>Verlierer</translation>
+    </message>
+</context>
+<context>
+    <name>LoadingIndicator</name>
+    <message>
+        <location filename="../qml/components/thirdparty/LoadingIndicator.qml" line="47"/>
+        <source>Loading...</source>
+        <translation>Laden...</translation>
+    </message>
+</context>
+<context>
+    <name>WatchlistPage</name>
+    <message>
+        <location filename="../qml/pages/WatchlistPage.qml" line="57"/>
+        <source>About</source>
+        <extracomment>WatchlistPage about menu item</extracomment>
+        <translation>Über</translation>
+    </message>
+    <message>
+        <location filename="../qml/pages/WatchlistPage.qml" line="62"/>
+        <source>Add stock</source>
+        <extracomment>WatchlistPage add stock menu item</extracomment>
+        <translation>Aktie hinzufügen</translation>
+    </message>
+    <message>
+        <location filename="../qml/pages/WatchlistPage.qml" line="69"/>
+        <source>Refresh all quotes</source>
+        <extracomment>WatchlistPage refresh all quotes menu item</extracomment>
+        <translation>Alle Kurse aktualisieren</translation>
+    </message>
+    <message>
+        <location filename="../qml/pages/WatchlistPage.qml" line="106"/>
+        <source>Stock quotes</source>
+        <extracomment>WatchlistPage page header</extracomment>
+        <translation>Aktienkurse</translation>
+    </message>
+    <message>
+        <location filename="../qml/pages/WatchlistPage.qml" line="134"/>
+        <source>Remove</source>
+        <extracomment>WatchlistPage remove menu item</extracomment>
+        <translation>Entfernen</translation>
+    </message>
+    <message>
+        <location filename="../qml/pages/WatchlistPage.qml" line="389"/>
+        <source>Network error</source>
+        <extracomment>WatchlistPage error message network error</extracomment>
+        <translation>Netzwerkfehler</translation>
+    </message>
+    <message>
+        <location filename="../qml/pages/WatchlistPage.qml" line="392"/>
+        <source>Some quotes not updated</source>
+        <extracomment>WatchlistPage error message some quotes</extracomment>
+        <translation>Einige Kurse wurden nicht aktualisiert</translation>
+    </message>
+    <message>
+        <location filename="../qml/pages/WatchlistPage.qml" line="448"/>
+        <source>hh:mm</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../qml/pages/WatchlistPage.qml" line="450"/>
+        <source>dd.MM.yyyy</source>
+        <translation type="unfinished"></translation>
+    </message>
+</context>
+<context>
+    <name>database</name>
+    <message>
+        <location filename="../qml/js/database.js" line="110"/>
+        <source>Stock added</source>
+        <translation>Aktie hinzugefügt</translation>
+    </message>
+    <message>
+        <location filename="../qml/js/database.js" line="112"/>
+        <source>Error adding stock</source>
+        <translation>Fehler beim Hinzufügen der Aktie</translation>
+    </message>
+</context>
+</TS>
---
+++ translations/harbour-watchlist.ts
@@ -0,0 +1,167 @@
+<?xml version="1.0" encoding="utf-8"?>
+<!DOCTYPE TS>
+<TS version="2.1">
+<context>
+    <name>AboutPage</name>
+    <message>
+        <location filename="../qml/pages/AboutPage.qml" line="36"/>
+        <source>Reset Database</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../qml/pages/AboutPage.qml" line="49"/>
+        <location filename="../qml/pages/AboutPage.qml" line="62"/>
+        <source>About Watchlist</source>
+        <extracomment>AboutPage title - header
+----------
+AboutPage title - about text title</extracomment>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../qml/pages/AboutPage.qml" line="64"/>
+        <source>This is app is a simple stock watchlist for Sailfish OS. Watchlist is open source and licensed under the GPL v3.</source>
+        <extracomment>AboutPage text - about text</extracomment>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../qml/pages/AboutPage.qml" line="74"/>
+        <source>Version</source>
+        <extracomment>AboutPage version label</extracomment>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../qml/pages/AboutPage.qml" line="85"/>
+        <source>Author</source>
+        <extracomment>AboutPage author label</extracomment>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../qml/pages/AboutPage.qml" line="106"/>
+        <source>Source code</source>
+        <extracomment>AboutPage about source label</extracomment>
+        <translation type="unfinished"></translation>
+    </message>
+</context>
+<context>
+    <name>AddStockPage</name>
+    <message>
+        <location filename="../qml/pages/AddStockPage.qml" line="63"/>
+        <source>Network error</source>
+        <extracomment>AddStockPage network error</extracomment>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../qml/pages/AddStockPage.qml" line="106"/>
+        <source>Search Results</source>
+        <extracomment>AddStockPage search result header</extracomment>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../qml/pages/AddStockPage.qml" line="113"/>
+        <source>Find your Stock...</source>
+        <extracomment>AddStockPage search result input field</extracomment>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../qml/pages/AddStockPage.qml" line="149"/>
+        <source>No results found</source>
+        <extracomment>AddStockPage no results label</extracomment>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../qml/pages/AddStockPage.qml" line="186"/>
+        <source>Add</source>
+        <extracomment>AddStockPage add menu item</extracomment>
+        <translation type="unfinished"></translation>
+    </message>
+</context>
+<context>
+    <name>CoverPage</name>
+    <message>
+        <location filename="../qml/cover/CoverPage.qml" line="95"/>
+        <source>Top</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../qml/cover/CoverPage.qml" line="95"/>
+        <source>Flop</source>
+        <translation type="unfinished"></translation>
+    </message>
+</context>
+<context>
+    <name>LoadingIndicator</name>
+    <message>
+        <location filename="../qml/components/thirdparty/LoadingIndicator.qml" line="47"/>
+        <source>Loading...</source>
+        <translation type="unfinished"></translation>
+    </message>
+</context>
+<context>
+    <name>WatchlistPage</name>
+    <message>
+        <location filename="../qml/pages/WatchlistPage.qml" line="57"/>
+        <source>About</source>
+        <extracomment>WatchlistPage about menu item</extracomment>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../qml/pages/WatchlistPage.qml" line="62"/>
+        <source>Add stock</source>
+        <extracomment>WatchlistPage add stock menu item</extracomment>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../qml/pages/WatchlistPage.qml" line="69"/>
+        <source>Refresh all quotes</source>
+        <extracomment>WatchlistPage refresh all quotes menu item</extracomment>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../qml/pages/WatchlistPage.qml" line="106"/>
+        <source>Stock quotes</source>
+        <extracomment>WatchlistPage page header</extracomment>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../qml/pages/WatchlistPage.qml" line="134"/>
+        <source>Remove</source>
+        <extracomment>WatchlistPage remove menu item</extracomment>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../qml/pages/WatchlistPage.qml" line="389"/>
+        <source>Network error</source>
+        <extracomment>WatchlistPage error message network error</extracomment>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../qml/pages/WatchlistPage.qml" line="392"/>
+        <source>Some quotes not updated</source>
+        <extracomment>WatchlistPage error message some quotes</extracomment>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../qml/pages/WatchlistPage.qml" line="448"/>
+        <source>hh:mm</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../qml/pages/WatchlistPage.qml" line="450"/>
+        <source>dd.MM.yyyy</source>
+        <translation type="unfinished"></translation>
+    </message>
+</context>
+<context>
+    <name>database</name>
+    <message>
+        <location filename="../qml/js/database.js" line="110"/>
+        <source>Stock added</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../qml/js/database.js" line="112"/>
+        <source>Error adding stock</source>
+        <translation type="unfinished"></translation>
+    </message>
+</context>
+</TS>
