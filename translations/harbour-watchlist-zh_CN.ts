aa071f42701365718de8c39e0e3f24fdff976bb6
dashinfantry <45334556+dashinfantry@users.noreply.github.com>
2019-09-05 10:52:04 +0800

    Create harbour-watchlist-zh_CN.ts

---
+++ translations/harbour-watchlist-zh_CN.ts
@@ -0,0 +1,167 @@
+<?xml version="1.0" encoding="utf-8"?>
+<!DOCTYPE TS>
+<TS version="2.1">
+<context>
+    <name>AboutPage</name>
+    <message>
+        <location filename="../qml/pages/AboutPage.qml" line="36"/>
+        <source>Reset Database</source>
+        <translation>重置数据库</translation>
+    </message>
+    <message>
+        <location filename="../qml/pages/AboutPage.qml" line="49"/>
+        <location filename="../qml/pages/AboutPage.qml" line="62"/>
+        <source>About Watchlist</source>
+        <extracomment>AboutPage title - header
+----------
+AboutPage title - about text title</extracomment>
+        <translation>关于 Wathlist</translation>
+    </message>
+    <message>
+        <location filename="../qml/pages/AboutPage.qml" line="64"/>
+        <source>This is app is a simple stock watchlist for Sailfish OS. Watchlist is open source and licensed under the GPL v3.</source>
+        <extracomment>AboutPage text - about text</extracomment>
+        <translation>这是一个简单的旗鱼系统股票清单软件。Watchlist 在 GPL v3 许可协议之下开源。</translation>
+    </message>
+    <message>
+        <location filename="../qml/pages/AboutPage.qml" line="74"/>
+        <source>Version</source>
+        <extracomment>AboutPage version label</extracomment>
+        <translation>版本</translation>
+    </message>
+    <message>
+        <location filename="../qml/pages/AboutPage.qml" line="85"/>
+        <source>Author</source>
+        <extracomment>AboutPage author label</extracomment>
+        <translation>作者</translation>
+    </message>
+    <message>
+        <location filename="../qml/pages/AboutPage.qml" line="106"/>
+        <source>Source code</source>
+        <extracomment>AboutPage about source label</extracomment>
+        <translation>源代码</translation>
+    </message>
+</context>
+<context>
+    <name>AddStockPage</name>
+    <message>
+        <location filename="../qml/pages/AddStockPage.qml" line="63"/>
+        <source>Network error</source>
+        <extracomment>AddStockPage network error</extracomment>
+        <translation>网络错误</translation>
+    </message>
+    <message>
+        <location filename="../qml/pages/AddStockPage.qml" line="106"/>
+        <source>Search Results</source>
+        <extracomment>AddStockPage search result header</extracomment>
+        <translation>搜索结果</translation>
+    </message>
+    <message>
+        <location filename="../qml/pages/AddStockPage.qml" line="113"/>
+        <source>Find your Stock...</source>
+        <extracomment>AddStockPage search result input field</extracomment>
+        <translation>找到你的股票……</translation>
+    </message>
+    <message>
+        <location filename="../qml/pages/AddStockPage.qml" line="149"/>
+        <source>No results found</source>
+        <extracomment>AddStockPage no results label</extracomment>
+        <translation>没有找到结果</translation>
+    </message>
+    <message>
+        <location filename="../qml/pages/AddStockPage.qml" line="186"/>
+        <source>Add</source>
+        <extracomment>AddStockPage add menu item</extracomment>
+        <translation>添加</translation>
+    </message>
+</context>
+<context>
+    <name>CoverPage</name>
+    <message>
+        <location filename="../qml/cover/CoverPage.qml" line="95"/>
+        <source>Top</source>
+        <translation>顶部</translation>
+    </message>
+    <message>
+        <location filename="../qml/cover/CoverPage.qml" line="95"/>
+        <source>Flop</source>
+        <translation>下跌</translation>
+    </message>
+</context>
+<context>
+    <name>LoadingIndicator</name>
+    <message>
+        <location filename="../qml/components/thirdparty/LoadingIndicator.qml" line="47"/>
+        <source>Loading...</source>
+        <translation>正在加载……</translation>
+    </message>
+</context>
+<context>
+    <name>WatchlistPage</name>
+    <message>
+        <location filename="../qml/pages/WatchlistPage.qml" line="57"/>
+        <source>About</source>
+        <extracomment>WatchlistPage about menu item</extracomment>
+        <translation>关于</translation>
+    </message>
+    <message>
+        <location filename="../qml/pages/WatchlistPage.qml" line="62"/>
+        <source>Add stock</source>
+        <extracomment>WatchlistPage add stock menu item</extracomment>
+        <translation>添加股票</translation>
+    </message>
+    <message>
+        <location filename="../qml/pages/WatchlistPage.qml" line="69"/>
+        <source>Refresh all quotes</source>
+        <extracomment>WatchlistPage refresh all quotes menu item</extracomment>
+        <translation>刷新全部报价</translation>
+    </message>
+    <message>
+        <location filename="../qml/pages/WatchlistPage.qml" line="106"/>
+        <source>Stock quotes</source>
+        <extracomment>WatchlistPage page header</extracomment>
+        <translation>股票报价</translation>
+    </message>
+    <message>
+        <location filename="../qml/pages/WatchlistPage.qml" line="134"/>
+        <source>Remove</source>
+        <extracomment>WatchlistPage remove menu item</extracomment>
+        <translation>移除</translation>
+    </message>
+    <message>
+        <location filename="../qml/pages/WatchlistPage.qml" line="399"/>
+        <source>Network error</source>
+        <extracomment>WatchlistPage error message network error</extracomment>
+        <translation>网络错误</translation>
+    </message>
+    <message>
+        <location filename="../qml/pages/WatchlistPage.qml" line="402"/>
+        <source>Some quotes not updated</source>
+        <extracomment>WatchlistPage error message some quotes</extracomment>
+        <translation>某些报价未更新</translation>
+    </message>
+    <message>
+        <location filename="../qml/pages/WatchlistPage.qml" line="458"/>
+        <source>hh:mm</source>
+        <translation>hh:mm</translation>
+    </message>
+    <message>
+        <location filename="../qml/pages/WatchlistPage.qml" line="460"/>
+        <source>dd.MM.yyyy</source>
+        <translation>dd.MM.yyy</translation>
+    </message>
+</context>
+<context>
+    <name>database</name>
+    <message>
+        <location filename="../qml/js/database.js" line="110"/>
+        <source>Stock added</source>
+        <translation>已添加股票</translation>
+    </message>
+    <message>
+        <location filename="../qml/js/database.js" line="112"/>
+        <source>Error adding stock</source>
+        <translation>加载股票出错</translation>
+    </message>
+</context>
+</TS>
