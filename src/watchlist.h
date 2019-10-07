fdbccc57852f0adb29aa2df96fedde426b9c064f
andywuest <andreas.wuest.freelancer@gmail.com>
2019-10-07 22:13:38 +0200

    added copyright header; cleanup

--- src/watchlist.h
+++ src/watchlist.h
@@ -1,3 +1,20 @@
+/*
+ * harbour-watchlist - Sailfish OS Version
+ * Copyright © 2019 Andreas Wüst (andreas.wuest.freelancer@gmail.com)
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program. If not, see <http://www.gnu.org/licenses/>.
+ */
 #ifndef WATCHLIST_H
 #define WATCHLIST_H
 
@@ -7,8 +24,7 @@
 
 #include "euroinvestorbackend.h"
 
-class Watchlist : public QObject
-{
+class Watchlist : public QObject {
     Q_OBJECT
 public:
     explicit Watchlist(QObject *parent = nullptr);
@@ -19,7 +35,7 @@ signals:
 
 public slots:
 
-private:private:
+private:
     QNetworkAccessManager *networkAccessManager;
     EuroinvestorBackend *euroinvestorBackend;
     QSettings settings;
