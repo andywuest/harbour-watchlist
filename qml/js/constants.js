4936932993c70349b6d7f4e59b907ac93b0a8649
andywuest <andreas.wuest.freelancer@gmail.com>
2019-10-21 21:29:33 +0200

    update

--- qml/js/constants.js
+++ qml/js/constants.js
@@ -4,9 +4,7 @@ var loggingEnabled = true;
 var POSITIVE_COLOR = '#00FF00';
 var NEGATIVE_COLOR = '#FF0000';
 
-var VERSION = "0.0.1"
-// according the QT Bug timeout in XMLHttpRequest is not supported
-var HTTP_TIMEOUT_IN_SECONDS = 15;
+var VERSION = "0.2.0"
 
 var SQL_TRUE = 1;
 var SQL_FALSE = 0;
