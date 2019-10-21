803c59c8bd89ce572f762b10f3536947dafff740
andywuest <andreas.wuest.freelancer@gmail.com>
2019-10-21 21:18:08 +0200

    added quite some translations

--- translations/harbour-watchlist-de.ts
+++ translations/harbour-watchlist-de.ts
@@ -51,31 +51,25 @@ AboutPage title - about text title</extracomment>
 <context>
     <name>AddStockPage</name>
     <message>
-        <location filename="../qml/pages/AddStockPage.qml" line="63"/>
-        <source>Network error</source>
-        <extracomment>AddStockPage network error</extracomment>
-        <translation>Netzwerkfehler</translation>
-    </message>
-    <message>
-        <location filename="../qml/pages/AddStockPage.qml" line="106"/>
+        <location filename="../qml/pages/AddStockPage.qml" line="71"/>
         <source>Search Results</source>
         <extracomment>AddStockPage search result header</extracomment>
         <translation>Suchergebnisse</translation>
     </message>
     <message>
-        <location filename="../qml/pages/AddStockPage.qml" line="113"/>
+        <location filename="../qml/pages/AddStockPage.qml" line="78"/>
         <source>Find your Stock...</source>
         <extracomment>AddStockPage search result input field</extracomment>
         <translation>Finde deine Aktie...</translation>
     </message>
     <message>
-        <location filename="../qml/pages/AddStockPage.qml" line="149"/>
+        <location filename="../qml/pages/AddStockPage.qml" line="114"/>
         <source>No results found</source>
         <extracomment>AddStockPage no results label</extracomment>
         <translation>Keine Ergebnisse gefunden</translation>
     </message>
     <message>
-        <location filename="../qml/pages/AddStockPage.qml" line="186"/>
+        <location filename="../qml/pages/AddStockPage.qml" line="151"/>
         <source>Add</source>
         <extracomment>AddStockPage add menu item</extracomment>
         <translation>Hinzufügen</translation>
@@ -84,12 +78,12 @@ AboutPage title - about text title</extracomment>
 <context>
     <name>CoverPage</name>
     <message>
-        <location filename="../qml/cover/CoverPage.qml" line="95"/>
+        <location filename="../qml/cover/CoverPage.qml" line="98"/>
         <source>Top</source>
         <translation>Gewinner</translation>
     </message>
     <message>
-        <location filename="../qml/cover/CoverPage.qml" line="95"/>
+        <location filename="../qml/cover/CoverPage.qml" line="98"/>
         <source>Flop</source>
         <translation>Verlierer</translation>
     </message>
@@ -103,71 +97,152 @@ AboutPage title - about text title</extracomment>
     </message>
 </context>
 <context>
+    <name>StockAlarmDialog</name>
+    <message>
+        <location filename="../qml/pages/StockAlarmDialog.qml" line="42"/>
+        <source>Configure Alarms</source>
+        <translation>Alarm konfigurieren</translation>
+    </message>
+    <message>
+        <location filename="../qml/pages/StockAlarmDialog.qml" line="43"/>
+        <source>Triggers a notification when the price of the stock is over the configured maximum price or under the configured minimum price.</source>
+        <translation>Löst einen Alarm aus, wenn der Preis der Aktie über dem konfigurierten Maximum-Kurs oder unter dem konfigurierten Minimum-Kurs liegt.</translation>
+    </message>
+    <message>
+        <location filename="../qml/pages/StockAlarmDialog.qml" line="51"/>
+        <source>Configuration hint</source>
+        <translation>Konfigurationshinweis</translation>
+    </message>
+    <message>
+        <location filename="../qml/pages/StockAlarmDialog.qml" line="69"/>
+        <source>Prices</source>
+        <translation>Kurse</translation>
+    </message>
+    <message>
+        <location filename="../qml/pages/StockAlarmDialog.qml" line="77"/>
+        <source>Minimum price</source>
+        <translation>Minimum Kurs</translation>
+    </message>
+    <message>
+        <location filename="../qml/pages/StockAlarmDialog.qml" line="88"/>
+        <source>Maximum price</source>
+        <translation>Maximum Kurs</translation>
+    </message>
+    <message>
+        <location filename="../qml/pages/StockAlarmDialog.qml" line="108"/>
+        <source>The latest known price for the stock was %0 %1. The alarm will be disabled once the alaram has been triggered. In order to activate the alarm again, you have to save the alarm again.</source>
+        <translation>Der letzte bekannte Kurs der Aktie war %0 %1. Der Alarm wird deaktiviert sobald er einmal ausgelöst wurde. Um den Alarm wieder zu aktivieren, muss dieser einfach noch einmal neu gespeichert werden.</translation>
+    </message>
+</context>
+<context>
     <name>WatchlistPage</name>
     <message>
-        <location filename="../qml/pages/WatchlistPage.qml" line="57"/>
+        <location filename="../qml/pages/WatchlistPage.qml" line="66"/>
         <source>About</source>
         <extracomment>WatchlistPage about menu item</extracomment>
         <translation>Über</translation>
     </message>
     <message>
-        <location filename="../qml/pages/WatchlistPage.qml" line="62"/>
+        <location filename="../qml/pages/WatchlistPage.qml" line="71"/>
         <source>Add stock</source>
         <extracomment>WatchlistPage add stock menu item</extracomment>
         <translation>Aktie hinzufügen</translation>
     </message>
     <message>
-        <location filename="../qml/pages/WatchlistPage.qml" line="69"/>
+        <location filename="../qml/pages/WatchlistPage.qml" line="78"/>
         <source>Refresh all quotes</source>
         <extracomment>WatchlistPage refresh all quotes menu item</extracomment>
         <translation>Alle Kurse aktualisieren</translation>
     </message>
     <message>
-        <location filename="../qml/pages/WatchlistPage.qml" line="106"/>
+        <location filename="../qml/pages/WatchlistPage.qml" line="115"/>
         <source>Stock quotes</source>
         <extracomment>WatchlistPage page header</extracomment>
         <translation>Aktienkurse</translation>
     </message>
     <message>
-        <location filename="../qml/pages/WatchlistPage.qml" line="134"/>
+        <location filename="../qml/pages/WatchlistPage.qml" line="143"/>
+        <source>Configure alarm</source>
+        <extracomment>WatchlistPage configure alarm menu item</extracomment>
+        <translation>Alarm konfigurieren</translation>
+    </message>
+    <message>
+        <location filename="../qml/pages/WatchlistPage.qml" line="151"/>
         <source>Remove</source>
         <extracomment>WatchlistPage remove menu item</extracomment>
         <translation>Entfernen</translation>
     </message>
     <message>
-        <location filename="../qml/pages/WatchlistPage.qml" line="401"/>
-        <source>Network error</source>
-        <extracomment>WatchlistPage error message network error</extracomment>
-        <translation>Netzwerkfehler</translation>
+        <location filename="../qml/pages/WatchlistPage.qml" line="414"/>
+        <source>The share has just dropped below %1.</source>
+        <translation>Der Kurs der Aktie ist gerade unter %1 gefallen.</translation>
     </message>
     <message>
-        <location filename="../qml/pages/WatchlistPage.qml" line="404"/>
-        <source>Some quotes not updated</source>
-        <extracomment>WatchlistPage error message some quotes</extracomment>
-        <translation>Einige Kurse wurden nicht aktualisiert</translation>
+        <location filename="../qml/pages/WatchlistPage.qml" line="421"/>
+        <source>The share has just risen above %1.</source>
+        <translation>Der Kurs der Aktie ist gerade über %1 gestiegen.</translation>
     </message>
     <message>
-        <location filename="../qml/pages/WatchlistPage.qml" line="460"/>
-        <source>hh:mm</source>
-        <translation type="unfinished"></translation>
-    </message>
-    <message>
-        <location filename="../qml/pages/WatchlistPage.qml" line="462"/>
-        <source>dd.MM.yyyy</source>
-        <translation type="unfinished"></translation>
+        <location filename="../qml/pages/WatchlistPage.qml" line="413"/>
+        <location filename="../qml/pages/WatchlistPage.qml" line="420"/>
+        <source>%1</source>
+        <translation>%1</translation>
     </message>
 </context>
 <context>
     <name>database</name>
     <message>
-        <location filename="../qml/js/database.js" line="110"/>
+        <location filename="../qml/js/database.js" line="152"/>
+        <source>Alarm added</source>
+        <translation>Alarm hinzugefügt</translation>
+    </message>
+    <message>
+        <location filename="../qml/js/database.js" line="154"/>
+        <source>Error adding alarm</source>
+        <translation>Fehler beim Hinzufügen des Alarms</translation>
+    </message>
+    <message>
+        <location filename="../qml/js/database.js" line="169"/>
+        <source>Alarm disabled</source>
+        <translation>Alarm deaktiviert</translation>
+    </message>
+    <message>
+        <location filename="../qml/js/database.js" line="171"/>
+        <source>Error disabling alarm</source>
+        <translation>Fehler beim deaktivieren des Alarms</translation>
+    </message>
+    <message>
+        <location filename="../qml/js/database.js" line="187"/>
+        <source>Alarm deleted</source>
+        <translation>Alarm gelöscht</translation>
+    </message>
+    <message>
+        <location filename="../qml/js/database.js" line="189"/>
+        <source>Error deleting alarm</source>
+        <translation>Fehler beim Löschen des Alarms</translation>
+    </message>
+    <message>
+        <location filename="../qml/js/database.js" line="245"/>
         <source>Stock added</source>
         <translation>Aktie hinzugefügt</translation>
     </message>
     <message>
-        <location filename="../qml/js/database.js" line="112"/>
+        <location filename="../qml/js/database.js" line="247"/>
         <source>Error adding stock</source>
         <translation>Fehler beim Hinzufügen der Aktie</translation>
+    </message>
+</context>
+<context>
+    <name>functions</name>
+    <message>
+        <location filename="../qml/js/functions.js" line="67"/>
+        <source>hh:mm</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../qml/js/functions.js" line="69"/>
+        <source>dd.MM.yyyy</source>
+        <translation type="unfinished"></translation>
     </message>
 </context>
 </TS>
