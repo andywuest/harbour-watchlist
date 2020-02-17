3889f39c927b8dcad4ca86b309ced27125fd8b07
andywuest <andreas.wuest.freelancer@gmail.com>
2020-02-17 21:33:01 +0100

    added pngcrush optimizer

--- export_app_icons.sh
+++ export_app_icons.sh
@@ -1,1 +1,7 @@
-for x in 86 256 128 108 ; do inkscape --export-png icons/${x}x${x}/harbour-watchlist.png -w ${x} mediasrc/harbour-watchlist.svg ; done
+!/bin/bash
+
+for x in 86 256 128 108
+do
+  inkscape --export-png icons/${x}x${x}/harbour-watchlist_original.png -w ${x} mediasrc/harbour-watchlist.svg
+  pngcrush -brute icons/${x}x${x}/harbour-watchlist_original.png icons/${x}x${x}/harbour-watchlist.png
+done
