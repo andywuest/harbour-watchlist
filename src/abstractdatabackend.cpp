5e3f1bedcd1f269e988ac9f1041b26766ed433ed
andywuest <andreas.wuest.freelancer@gmail.com>
2020-03-28 22:54:15 +0100

    added abstract backend

---
+++ src/abstractdatabackend.cpp
@@ -0,0 +1,70 @@
+/*
+ * harbour-watchlist - Sailfish OS Version
+ * Copyright © 2020 Andreas Wüst (andreas.wuest.freelancer@gmail.com)
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program. If not, see <http://www.gnu.org/licenses/>.
+ */
+#include "abstractdatabackend.h"
+
+#include <QDebug>
+#include <QFile>
+#include <QUrl>
+#include <QUrlQuery>
+#include <QUuid>
+#include <QJsonObject>
+#include <QJsonArray>
+#include <QDateTime>
+#include <QVariantMap>
+#include <QJsonDocument>
+
+AbstractDataBackend::AbstractDataBackend(QNetworkAccessManager *manager, const QString &applicationName, const QString applicationVersion, QObject *parent) : QObject(parent) {
+    qDebug() << "Initializing Data Backend...";
+    this->manager = manager;
+    this->applicationName = applicationName;
+    this->applicationVersion = applicationVersion;
+}
+
+AbstractDataBackend::~AbstractDataBackend() {
+    qDebug() << "Shutting down Euroinvestor Backend...";
+}
+
+QNetworkReply *AbstractDataBackend::executeGetRequest(const QUrl &url) {
+    qDebug() << "AbstractDataBackend::executeGetRequest " << url;
+    QNetworkRequest request(url);
+    request.setHeader(QNetworkRequest::ContentTypeHeader, MIME_TYPE_JSON);
+    request.setHeader(QNetworkRequest::UserAgentHeader, USER_AGENT);
+
+    return manager->get(request);
+}
+
+void AbstractDataBackend::handleRequestError(QNetworkReply::NetworkError error) {
+    QNetworkReply *reply = qobject_cast<QNetworkReply *>(sender());
+    qWarning() << "AbstractDataBackend::handleRequestError:" << (int)error << reply->errorString() << reply->readAll();
+
+    emit requestError("Return code: " + QString::number((int)error) + " - " + reply->errorString());
+}
+
+QDate AbstractDataBackend::getStartDateForChart(const int chartType) {
+    QDate today = QDate::currentDate();
+    QDate startDate;
+    switch(chartType) {
+        case ChartType::INTRADAY: break;
+        case ChartType::MONTH: startDate = today.addMonths(-1); break;
+        case ChartType::THREE_MONTHS: startDate = today.addMonths(-3); break;
+        case ChartType::YEAR: startDate = today.addYears(-1); break;
+        case ChartType::THREE_YEARS: startDate = today.addYears(-3); break;
+        case ChartType::FIVE_YEARS: startDate = today.addYears(-5); break;
+    }
+    return startDate;
+}
---
+++ src/abstractdatabackend.h
@@ -0,0 +1,71 @@
+/*
+ * harbour-watchlist - Sailfish OS Version
+ * Copyright © 2019 Andreas Wüst (andreas.wuest.freelancer@gmail.com)
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program. If not, see <http://www.gnu.org/licenses/>.
+ */
+#ifndef ABSTRACTDATABACKEND_H
+#define ABSTRACTDATABACKEND_H
+
+#include <QObject>
+#include <QNetworkReply>
+#include <QNetworkAccessManager>
+
+const char MIME_TYPE_JSON[] = "application/json";
+const char USER_AGENT[] = "Mozilla/5.0 (X11; Ubuntu; Linux x86_64; rv:73.0) Gecko/20100101 Firefox/73.0";
+
+class AbstractDataBackend : public QObject {
+  Q_OBJECT
+public:
+    explicit AbstractDataBackend(QNetworkAccessManager *manager, const QString &applicationName, const QString applicationVersion, QObject *parent = 0);
+    ~AbstractDataBackend();
+
+    Q_INVOKABLE virtual void searchName(const QString &searchString) = 0;
+    Q_INVOKABLE virtual void searchQuote(const QString &searchString) = 0;
+    Q_INVOKABLE virtual void fetchPricesForChart(const QString &extRefId, const int chartType) = 0;
+    Q_INVOKABLE virtual bool isChartTypeSupported(const int chartType) = 0;
+
+    // signals for the qml part
+    Q_SIGNAL void searchResultAvailable(const QString &reply);
+    Q_SIGNAL void quoteResultAvailable(const QString &reply);
+    Q_SIGNAL void fetchPricesForChartAvailable(const QString &reply, const int chartType);
+    Q_SIGNAL void requestError(const QString &errorMessage);
+
+protected:
+
+    QString applicationName;
+    QString applicationVersion;
+    QNetworkAccessManager *manager;
+
+    enum ChartType {
+      INTRADAY = 0,
+      MONTH = 1,
+      THREE_MONTHS = 2,
+      YEAR = 3,
+      THREE_YEARS = 4,
+      FIVE_YEARS = 5
+    };
+
+    virtual QString convertCurrency(const QString &currencyString) = 0;
+
+    QNetworkReply *executeGetRequest(const QUrl &url);
+    QDate getStartDateForChart(const int chartType);
+
+protected slots:
+
+    void handleRequestError(QNetworkReply::NetworkError error);
+
+};
+
+#endif // ABSTRACTDATABACKEND_H
