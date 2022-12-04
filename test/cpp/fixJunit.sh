5afed83a177a47069ae83b50261472b7d055a629
Andreas Wüst <andreas.wuest.freelancer@gmail.com>
2022-12-04 15:17:30 +0100

    further fix;

--- test/cpp/fixJunit.sh
+++ test/cpp/fixJunit.sh
@@ -2,5 +2,5 @@
 
 # workaround for not properly generated junit xml files by qt test
 sed -i -e 's/<\/testsuite>/<\/testsuite><\/testsuites>/g' junit.xml
-sed -i -e 's/<testsuite /<testsuites name="workaround" time="0.0"><testsuite /g' junit.xml
+sed -i -e 's/<testsuite /<testsuites name="workaround" time="0.0"><testsuite time="0.0" /g' junit.xml
 cat *.xml
