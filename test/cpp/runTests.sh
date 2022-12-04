f96924e835c0d0ae8cfc1becdd73d4ac7d57cd72
Andreas Wüst <andreas.wuest.freelancer@gmail.com>
2022-12-04 15:20:51 +0100

    consolidation;

--- test/cpp/runTests.sh
+++ test/cpp/runTests.sh
@@ -9,7 +9,6 @@ find . -name "Makefile" -exec rm  {} \;
 
 qmake -o Makefile harbour-watchlist-tests.pro
 make
-# env LC_ALL=de_DE.UTF-8 LC_NUMERIC=de_DE.utf8 ./IngDibaBackendTest -o xmlresults.tap,tap
 env LC_ALL=de_DE.UTF-8 LC_NUMERIC=de_DE.utf8 ./IngDibaBackendTest -junitxml -o junit.xml
 
 
--- test/qml/runTests.sh
+++ test/qml/runTests.sh
@@ -2,5 +2,5 @@
 
 rm -rf *.xml
 
-env LC_ALL=de_DE.UTF-8 LC_NUMERIC=de_DE.utf8 qmltestrunner -platform offscreen -o qmlresults.tap,tap
+env LC_ALL=de_DE.UTF-8 LC_NUMERIC=de_DE.utf8 qmltestrunner -platform offscreen -junitxml -o junit.xml
 
